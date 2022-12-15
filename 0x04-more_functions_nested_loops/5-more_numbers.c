96266e774833b197cec1d4bee8faa05ff45fc4d6
neville chemwa <nevillechemwa@gmail.com>
2022-12-15 07:28:03 -0800

    prints 10 times the numbers, from 0 to 14, followed by a new line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,18 @@
  */
 void more_numbers(void)
 {
-	int a, b;
+	int x, y;
 
-	for (a = 0; a < 10; a++)
+	for (x = 0; x < 10; x++)
 	{
-		for (b = 0; b <= 14; b++)
+		for (y = 0; y <= 14; y++)
 		{
-			if (b > 9)
+			if (y > 9)
 			{
-				_putchar((b /10) + '0');
+				_putchar((y /10) + '0');
 			}
 
-			_putchar((b % 10) + '0');
+			_putchar((y % 10) + '0');
 		}
 
 		_putchar('\n);
