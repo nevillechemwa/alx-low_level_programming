50469548b79d310969de3945282ebc869d298020
neville chemwa <nevillechemwa@gmail.com>
2022-12-19 06:12:34 -0800

    generate password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,21 +7,6 @@
  * @s: input char
  * Return: checksum
  */
-unsigned long checksum(char *s)
-{
-unsigned long sum = 0;
-while (*s != 0)
-{
-	sum += *s;
-	s++;
-}
-return (sum);
-}
-/**
- * main - prints password for crakme
- *
- * Return: Always 0.
- */
 int main(void)
 {
 	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQSTUVWXYZ";
