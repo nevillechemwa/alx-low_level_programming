755471e65e2a42ced8461d12c55c7f6b79a6ed48
neville chemwa <nevillechemwa@gmail.com>
2022-12-19 05:15:58 -0800

    copy a string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,9 +8,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *aux = dest;
-
-	while (*src)
-		*dest++ = *src++;
-	return (aux);
+	int i;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i++] = '\0';
+	return (dest);
 }
