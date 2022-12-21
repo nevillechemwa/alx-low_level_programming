43e5572a026b3c1fab24a3188bb8f494cc0c0dcd
neville chemwa <nevillechemwa@gmail.com>
2022-12-21 10:18:31 -0800

    lowercase to upper

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,19 +10,32 @@ char *cap_string(char *s)
 {
 	int i = 0;
 
-	while (s[i] != 0)
+	while (s[i])
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-		{
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] -= 32;
-			else if (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1]
-					== '.' || s[i - 1] == '!' || s[i - 1] == '"'
-					|| s[i - 1] == ')' || s[i - 1] == '(' ||
-					s[i - 1] == '{' || s[i - 1] == '}' || s[i - 1]
-					== '\n' || s[i - 1] == 32 || s[i - 1] == '\t')
-				        s[i] -= 32;
-		}
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			s[i + 1] -= 32;
 		i++;
 	}
 	return (s);
+}
+
+/**
+ * check_seperators - Separators of words: space, tabulation, new line,
+ * ,, ;, ., !, ?, ", (, ), {, and }
+ * @c: an input character
+ * Return: 1 if seperator, 0 otherwise
+ */
+int check_seperators(char c)
+{
+	int i = 0;
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
+			return (1);
+	}
+	return (0);
 }
