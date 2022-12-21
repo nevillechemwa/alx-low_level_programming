4525e59d7a6dcdf7f43ceef0c3b8dfdc72530cd8
neville chemwa <nevillechemwa@gmail.com>
2022-12-21 00:30:12 -0800

    Reverses the content of an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * reverse_array - function that reverses an array
+ * @a: input array
+ * @n: number of elements of an array
+ * Return: Always 0
+ */
+void reverse_array(int *a, int n)
+{
+	int *p, i, aux, k;
+
+	p = a;
+
+	for (i = 1; i < n; i++)
+	{
+		p++;
+	}
+	for (k = 0; k < i / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
+}
