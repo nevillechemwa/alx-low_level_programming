226e40b41fed078b9721cbbfbea9ce292982fdcc
neville chemwa <nevillechemwa@gmail.com>
2022-12-21 11:58:44 -0800

    buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,46 +2,35 @@
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
+ * print_buffer - print bufer
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
  *
  * Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-        unsigned int i;
+void print_buffer(char *b, int size) {
+    int i, j;
 
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10) 
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
+    for (i = 0; i < size; i += 10) {
+        printf("%08x: ", i);
+        for (j = i; j < i + 10; j++) {
+            if (j < size) {
+                printf("%02x ", b[j]);
+            } else {
+                printf("   ");
+            }
         }
-        printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char buffer[98] = {0x00};
-
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
+        printf(" ");
+        for (j = i; j < i + 10; j++) {
+            if (j < size) {
+                if (isprint(b[j])) {
+                    putchar(b[j]);
+                } else {
+                    putchar('.');
+                }
+            }
+        }
+        putchar('\n');
+    }
 }
 
