f2adf786eade864c55c1262fd0ccf2fc4119d411
neville chemwa <nevillechemwa@gmail.com>
2022-12-22 01:51:26 -0800

    buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,35 +2,60 @@
 #include <stdio.h>
 
 /**
- * print_buffer - print bufer
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
  *
- * Return: Nothing.
+ * Return: void
  */
-void print_buffer(char *b, int size) {
-    int i, j;
 
-    for (i = 0; i < size; i += 10) {
-        printf("%08x: ", i);
-        for (j = i; j < i + 10; j++) {
-            if (j < size) {
-                printf("%02x ", b[j]);
-            } else {
-                printf("   ");
-            }
-        }
-        printf(" ");
-        for (j = i; j < i + 10; j++) {
-            if (j < size) {
-                if (isprint(b[j])) {
-                    putchar(b[j]);
-                } else {
-                    putchar('.');
-                }
-            }
-        }
-        putchar('\n');
-    }
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
 }
 
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
+}
