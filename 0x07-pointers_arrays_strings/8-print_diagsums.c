cfa7cb2f930fe3002675f66d92c7a17ead5e1107
neville chemwa <nevillechemwa@gmail.com>
2023-01-02 06:54:19 -0800

    prints the sum of the two diagonals of a square matrix of integers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagonals of a square matrix of integers
+ * print_diagsums - prints the sum of the two diagonals of a square matrix
  * @a: matrix
  * @size: size
  * Return: nothing
