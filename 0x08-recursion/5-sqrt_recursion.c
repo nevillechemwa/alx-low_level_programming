1103a9d37e49bafa8e8bca627771d7c9b6b3414f
neville chemwa <nevillechemwa@gmail.com>
2023-01-04 07:27:12 -0800

    returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,16 +2,15 @@
 int _sqrt(int prev, int root);
 
 /**
- * _sqrt_recursion - It returns the value of square root of n.
- * @n: an input integer
- * Return: The square root of n
+ * _sqrt_recursion - It returns the value of square root of n
+ *  @n: an input integer
+ *  Return: The square root of n
  */
 int _sqrt_recursion(int n)
 {
-        if (n < 0)
-                return (-1);
-
-        return (_sqrt(1, n));
+	if (n < 0)
+		return (-1);
+	return (_sqrt(1, n));
 }
 
 /**
@@ -22,10 +21,9 @@ int _sqrt_recursion(int n)
  */
 int _sqrt(int prev, int root)
 {
-        if (prev > root)
-                return (-1);
-        else if (prev * prev == root)
-                return (prev);
-        return (_sqrt(prev + 1, root));
+	if (prev > root)
+		return (-1);
+	else if (prev * prev == root)
+		return (prev);
+	return (_sqrt(prev + 1, root));
 }
-
