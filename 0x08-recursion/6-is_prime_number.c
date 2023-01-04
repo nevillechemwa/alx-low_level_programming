2e472d0497d99479b4efd2ef21df0802400297fa
neville chemwa <nevillechemwa@gmail.com>
2023-01-04 07:43:55 -0800

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+int is_prime_number_helper(int n, int divisor);
+
+/**
+ * is_prime_number - check if an integer is a prime number
+ * @n: integer to check
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	return (is_prime_number_helper(n, 2));
+}
+
+/**
+ * is_prime_number_helper - recursively check if n is divisible by divisor
+ * @n: integer to check
+ * @divisor: divisor to try
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number_helper(int n, int divisor)
+{
+	if (n == divisor)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (is_prime_number_helper(n, divisor + 1));
+}
