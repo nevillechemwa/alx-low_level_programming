a244f67ad97f4f58eb165abc086b84fe04e0362c
neville chemwa <nevillechemwa@gmail.com>
2023-01-04 05:03:59 -0800

    returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - calculate length of string
+ * @s: string to count
+ * Return: int value
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	else
+	{
+		return 1+ _strlen_recursion(s + 1);
+	}
+}
