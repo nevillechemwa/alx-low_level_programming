178973b1b37439a1bdf6aee271e05b360d45a2e9
neville chemwa <nevillechemwa@gmail.com>
2023-01-06 08:49:05 -0800

    prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguement it recieves
+ * @argc: arguement count
+ * @argv: arguement value
+ * Return: Always 0 (success)
+ */
+int main(int argc, char **argv[])
+{
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
+}
