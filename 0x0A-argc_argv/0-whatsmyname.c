f613face98133752028e1808f15716cdda39ef50
neville chemwa <nevillechemwa@gmail.com>
2023-01-06 08:30:47 -0800

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * @argc: arguement count
+ * @argv: an array of character strings containing the arguments
+ * Return: Always 0 (succes)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
