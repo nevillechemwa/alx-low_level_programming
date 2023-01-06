936804a4e2956ba45efe620fc7004fdafbe63c88
neville chemwa <nevillechemwa@gmail.com>
2023-01-06 10:48:14 -0800

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,21 +8,17 @@
  * @argv: The arguments' values
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
-	int num1, num2, prod;
+	int result;
 
-	if (argc != 3)
+	if (argc < 2)
 	{
-		if (argc != 3)
+		printf("Error\n");
 		return (1);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	prod = num1 * num2;
-
-	printf("%d\n", prod);
-
+	result = (int) *argv[1] * (int) *argv[2];
+	printf("%d\n", total);
 	return (0);
 }
