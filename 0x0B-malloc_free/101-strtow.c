9deac3f995682ef183a5e8397bebffe0e633459e
neville chemwa <nevillechemwa@gmail.com>
2023-01-09 14:04:07 -0800

    function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -81,9 +81,9 @@ char **strtow(char *str)
 			}
 			words[b][c] = '\0';
 			b++;
-		       	c = 0;
-		       	length = 0;
-		       	str++;
+		      	c = 0;
+		      	length = 0;
+		      	str++;
 		}
 	}
 	return (words);
