f19227bf5fc740b66dea42b370e70a784d1e6d7f
neville chemwa <nevillechemwa@gmail.com>
2023-01-09 12:31:31 -0800

    string concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,17 +11,39 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	char *concat = malloc(strlen(s1) + strlen(s2) + 1);
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
 
-	if (concat == NULL)
+	l = i + j;
+	s = malloc((sizeof(char) * l) + 1);
+
+	if (s == NULL)
 		return (NULL);
 
-	strcpy(concat, s1);
-	strcpy(concat, s2);
+	j = 0;
 
-	return concat;
+	while (k < l)
+	{
+		if (k <= i)
+			s[k] = s1[k];
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+
+		k++;
+	}
+
+	s[k] = '\0';
+	return (s);
 }
