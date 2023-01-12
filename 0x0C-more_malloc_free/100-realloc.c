84efbe758453971f8774d494fe7cde8730a12d0b
neville chemwa <nevillechemwa@gmail.com>
2023-01-12 04:25:46 -0800

    reallocating memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,24 +12,21 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
-	unsigned int min_size;
-
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr)
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+	}
 	if (new_size == old_size)
 	{
 		return (ptr);
 	}
-	if (ptr == NULL)
-	{
-		return (malloc(new_size));
-	}
-	if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	min_size = old_size < new_size ? old_size : new_size;
-	memcpy(new_ptr, ptr, min_size);
 	free(ptr);
-	return (new_ptr);
+	ptr = malloc(new_size);
+
+	return (ptr);
 }
