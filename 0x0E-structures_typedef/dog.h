1ecd8d9ff458ebdecb87851a026cd5b5c25612ec
neville chemwa <nevillechemwa@gmail.com>
2023-01-16 14:09:53 -0800

    dog_t as a new name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,7 @@ typedef struct dog
 	char *name;
 	char *owner;
 	float age;
-} dog_a;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, char *owner, float age);
 void print_dog(struct dog *d);
