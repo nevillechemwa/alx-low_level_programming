62cd83612090f601f500dbc563fa46406ec23807
neville chemwa <nevillechemwa@gmail.com>
2023-01-16 13:30:57 -0800

    initializing variable of struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,16 @@
  */
 void print_dog(struct dog *d)
 {
-	strcpy(d->name, name);
-	d->age = age;
-	strcpy(d->owner, owner);
+	if (d != NULL)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %f\n", d->age);
+		if (d->owner)
+			printf("owner: %s\n", d->owner);
+		else
+			printf("owner: (nil)\n");
+	}
 }
