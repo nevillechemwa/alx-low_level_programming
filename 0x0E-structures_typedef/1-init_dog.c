52913e8a5782eef0da5b6a9a65fa66f534aa4627
neville chemwa <nevillechemwa@gmail.com>
2023-01-16 13:39:51 -0800

    dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,14 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		d = malloc(sizeof(struct dog));
-		if (d == NULL)
-			return;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
 }
