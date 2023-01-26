2e1f2f71dffbaf175d4fc489bb2533cb8b75fbe5
nevillechemwa <nevillechemwa@gmail.com>
2023-01-26 03:06:06 -0800

    adds a new node at the beggining of a list_t

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,31 +2,33 @@
 
 /**
  * add_node - add a new node at the beginning of a list
- * @head: head of the list
+ * @head: head of the linked list
  * @str: string in the struct
+ *
  * Return: the address of the new element
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *newnode;
 	int newelem = 0;
 
-	new = malloc(sizeof(list_t))
+	newnode = malloc(sizeof(list_t));
 
-	if (new == NULL)
+	if (newnode == NULL)
 	{
 		return (NULL);
 	}
 
-	new->str = strdup(str);
+	newnode->str = strdup(str);
 
 	while (str[newelem])
 	{
 		newelem++;
 	}
-	new->len = newelem;
-	new->next = *head;
-	*head = new;
 
+	newnode->len = newelem;
+	newnode->next = *head;
+	*head = newnode;
 	return (*head);
 }
