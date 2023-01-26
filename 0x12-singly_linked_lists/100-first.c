b610a0e12dcac30c76ef4c4c1d372ccbea1d0e70
nevillechemwa <nevillechemwa@gmail.com>
2023-01-26 03:48:31 -0800

    prints before the main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,11 @@ void first(void) attribute((constructor));
  */
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-			"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
 }
