f26ab0a23fae4940a45206340fdeeea9d8aa89de
nevillechemwa <nevillechemwa@gmail.com>
2023-01-26 03:03:11 -0800

    adds a new node at the beggining of a list_t

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,15 +9,24 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	int newelem = 0;
 
 	new = malloc(sizeof(list_t))
+
 	if (new == NULL)
+	{
 		return (NULL);
+	}
 
 	new->str = strdup(str);
-	new->len = strlen(str);
+
+	while (str[newelem])
+	{
+		newelem++;
+	}
+	new->len = newelem;
 	new->next = *head;
 	*head = new;
 
-	return (new);
+	return (*head);
 }
