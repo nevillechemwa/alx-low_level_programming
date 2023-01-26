92e21eae27e051f0f898dcfe42062ba5bca5ec8b
nevillechemwa <nevillechemwa@gmail.com>
2023-01-26 02:23:02 -0800

    returns the number of elements in a linked list_t

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a 
+ * list_len - returns the number of elements in a
  * linked list_t list
  * @h: head of the list
  * Return: the number of elements
