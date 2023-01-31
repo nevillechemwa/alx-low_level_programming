86fa6e05d075fa18a81d841fdf66b2cdcb5ba2ff
nevillechemwa <nevillechemwa@gmail.com>
2023-01-31 08:29:30 -0800

    function that frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -17,10 +17,10 @@ void free_listint2(listint_t **head)
 
 	 current = *head;
 	 while (current != NULL)
-	{
+	 {
 		next = current->next;
 		free(current);
 		current = next;
-	}
+	 }
 	 *head = NULL;
 }
