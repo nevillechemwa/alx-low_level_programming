d62f656ef1c9f074e70669cbb8e65c9b9c8e927a
nevillechemwa <nevillechemwa@gmail.com>
2023-02-06 05:46:23 -0800

    function that reads a text file and prints it

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints 
+ * read_textfile - reads a text file and prints
  * it to the POSIX standard output.
  * @filename: name of the file
  * @letters: number of letters
